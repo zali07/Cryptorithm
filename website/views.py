6151fc2a9842ac234740cd964ed5f4f58bc2ba27
Fuzi Zalan <fuzi.zalan@student.ms.sapientia.ro>
2023-05-05 20:18:13 +0300

    connected ui with backend enc and dec, msg fade

--- website/views.py
+++ website/views.py
@@ -3,6 +3,8 @@ from flask_login import login_required, current_user
 from .models import Note
 from . import db
 import json
+from website.cryptorithms import encryption as enc
+from website.cryptorithms import decryption as dec
 
 views = Blueprint('views', __name__)
 
@@ -11,34 +13,42 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-        button1 = request.form.get('encrypt')
-        button2 = request.form.get('decrypt')
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
+        plaintext = request.form.get('ptext')
+        selecter = request.form.get('enty')
+        if len(plaintext) < 1:
+            flash('Note is too short!', category='error')
+        elif selecter == None:
+            flash('No algorithm is selected!', category='error')
         else:
-            if button1:
-                new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-                db.session.add(new_note) #adding the note to the database 
-                db.session.commit()
-                flash('Encrypted', category='success')
-            if button2:
-                new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-                db.session.add(new_note) #adding the note to the database 
-                db.session.commit()
-                flash('Decrypted', category='success')
-
+            match selecter:
+                case "CAESAR_EN":
+                    shift = request.form.get("CAESAR_EN_SHIFT")
+                    ciphertext = enc.caesar_encrypt(plaintext, int(shift))
+                case "AFFINE_EN":
+                    affine_en_a = request.form.get("AFFINE_EN_A")
+                    affine_en_b = request.form.get("AFFINE_EN_B")
+                    ciphertext = enc.affine_encrypt(plaintext, int(affine_en_a), int(affine_en_b))
+                case "CAESAR_DE":
+                    shift = request.form.get("CAESAR_DE_SHIFT")
+                    ciphertext = dec.caesar_decrypt(plaintext, int(shift))                
+                case "AFFINE_DE":
+                    affine_de_a = request.form.get("AFFINE_DE_A")
+                    affine_de_b = request.form.get("AFFINE_DE_B")
+                    ciphertext = dec.affine_decrypt(plaintext, int(affine_de_a), int(affine_de_b))
+            new_note = Note(data=ciphertext, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Securely Transmitted!', category='success')
     return render_template("home.html", user=current_user)
 
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    note = json.loads(request.data)
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
         if note.user_id == current_user.id:
             db.session.delete(note)
             db.session.commit()
-
     return jsonify({})
